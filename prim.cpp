67bf458160c94abd77ceab004f7cd9ff568ef164
tylerhooks <tah112@txstate.edu>
2016-04-27 16:19:52 -0500

    stuff

--- prim.cpp
+++ prim.cpp
@@ -1,99 +1,162 @@
 #include <iostream>
-#include <vector>
 #include <fstream>
-#include <string>
-#include <algorithm>
+#include <vector>
 #include "prim.h"
-
 using namespace std;
 
-int minIncident(vector< vector<int> > edgeSet, int , int m){
- 	vector <int> edge;
-  	int index = 0;
-  	int cost;
-  	
-  	for(int i = 0; i < m; i++){
-    if(k==edgeSet[i][0] || k == edgeSet[i][1]){
-      edge.push_back(i);
 
-     }
+int addVertex(vector< vector<int> > edgeSet, vector<int> VH, int row);
+int minIncidentSet(vector< vector<int> > edgeSet, vector<int> VH, int m, bool []);
+void checkIfTwice(vector<int> edges, bool arr[]);
+void displayTree(vector< vector<int> > edgeSet, vector<int> T);
 
-   }
+ifstream infile("exampleWG.txt");
 
-	cout << "The edges incident with VH are  " << endl;
+int main() {
+  
+  int n; // Number of vertices.
+  int m; // Number of edges.
+ 
+  int edge;
 
-	for(int i = 0; i < edge.size(); i++){
-    cout << "edge " << edgeSet[edge[i]][0] << " " <<edgeSet[edge[i]][1] << " with cost "<< edgeSet[edge[i]][2] << endl;
-  	}
+  infile >> n; // Reads in the number of vertices.
+  infile >> m; // Reads in the number of edges.
+ 
+  // Check that the order of the graph is correct
+  cout <<"The order of the graph is "<< n << endl;
 
-  	cost = edgeSet[edge[0]][2];
+  // Check that the size of the graph is correct
+  cout <<"The size of the graph is " <<  m << endl << endl;
 
+  /* 2-D vector that stores the edges
+  [[row 1], [row 2], ..., [row m]]*/
+  vector< vector<int> > edgeSet;
 
-  	for(int i = 1; i < edge.size(); i++){
-   	 if(edgeSet[edge[i]][2] < cost){
-     	 cost = edgeSet[edge[i]][2];
-      	 index = i;
+  for(int i = 0; i < m; i ++){
+    vector<int> row; // The i-th row vector will be the i-th row of .txt
+
+    for(int j = 0; j < 3; j++){
+      infile >> edge;
+      row.push_back(edge);
     }
+
+    edgeSet.push_back(row); // Places row in the i-th entry of edgeSet
   }
 
- index = edge[index];
- return index; 
+  vector <int> VH; // VH is the vertex set of our tree
+  
+  vector<int> T; // T is the edge set of the tree
+
+  bool *used = new bool[m];
+  
+  for(int i = 0; i < m; i++) {
+    used[i] = false;
+  }
+  
+  /* Initialize the vertex set of our tree (in this case vertex 0) */
+  VH.push_back(0);
+  int edges; // Min incident edge index to our initial vertex
+  int vertex;
+  
+  while(VH.size() < n) {
+    edges = minIncidentSet(edgeSet, VH, m, used);
+    used[edges] = true;
+    T.push_back(edges);
+    vertex = addVertex(edgeSet, VH, edges);
+    VH.push_back(vertex);
+  }
+  
+  cout << endl;
+  
+  displayTree(edgeSet, T);
+  
+  delete [] used;
+  
+  return 0;
 }
-int addVertex(vector<vector<int> >edgeSet, vector<vector<int> > T, vector<int> VH){
-  int addVertex;
-  int dontAdd = VH[0];
-
-  if(dontAdd == T[0][0]){
-    addVertex = T[0][1];
-  }
-  else if(dontAdd == T[0][1]){
-      addVertex = T[0][0];
-  }
 
 
-  return addVertex;
-}
-int minIncidentSet(vector<vector<int> > edgeSet, vector<int> VH, int m){
+int minIncidentSet(vector<vector<int> > edgeSet, vector<int> VH, int m, bool used[]){
 
-	vector <int> edge;
+  vector <int> edge;
   int index = 0;
   int cost;
   
-  for(int j = 0; j < VH.size();j++)
-  {
-    for(int i = 0; i < m; i++)
-    {
-      /*cout << "VH[ " << j <<" ] == edgeSet[" << i<< " ][0] && VH[j] != edgeSet[i][1]" << endl;*/
-      if((VH[j] == edgeSet[i][0] && VH[j] != edgeSet[i][1]) ||
-     (VH[j] != edgeSet[i][0] && VH[j] == edgeSet[i][1]))
-    {
-      edge.push_back(i);
-    }
+  for(int j = 0; j < VH.size();j++) {
+    for(int i = 0; i < m; i++) {
+      if(((VH[j] == edgeSet[i][0] && VH[j] != edgeSet[i][1]) ||
+      (VH[j] != edgeSet[i][0] && VH[j] == edgeSet[i][1])) && !(used[i])) {
+        edge.push_back(i);
+      }
     }
   }
 
+  bool *arr = new bool[edge.size()];
+  
+  for(int i = 0; i < edge.size(); i++) {
+    arr[i] = false;
+  }
 
+  Prim p;
+  p.checkIfTwice(edge, arr);
+  
   cout << "The edges incident with VH are  " << endl;
 
-
-  for(int i = 0; i < edge.size(); i++){
-    cout << "edge " << edgeSet[edge[i]][0] << " " <<edgeSet[edge[i]][1] << " with cost "<< edgeSet[edge[i]][2] << endl;
+  for(int i = 0; i < edge.size(); i++) {
+    if(!(arr[i])) {
+      cout << "edge " << edgeSet[edge[i]][0] << " " << edgeSet[edge[i]][1];
+      cout << " with cost "<< edgeSet[edge[i]][2] << endl;
+    }
   }
 
-  cost = edgeSet[edge[0]][2];
+  cost = 10000000;
 
-
-  for(int i = 1; i < edge.size(); i++){
-    if(edgeSet[edge[i]][2] < cost){
+  for(int i = 0; i < edge.size(); i++) {
+    if(edgeSet[edge[i]][2] < cost && !(arr[i])) {
       cost = edgeSet[edge[i]][2];
       index = i;
     }
   }
 
+ cout << "Adding edge " << edge[index] << " with cost " << cost << endl;
 
- cout <<"Edge " <<  edge[index] <<" with cost "<< cost << endl;
-
- // cout <<"We add vertex " << edgeSet[edge[index]][0]<< endl;
  index = edge[index];
- return index; 
+
+ return index;
+}
+int addVertex(vector<vector<int> > edgeSet, vector<int> VH, int row){
+
+  for(int i = 0; i < VH.size(); i++) {
+    if(VH[i] == edgeSet[row][0]) {
+      return edgeSet[row][1];
+    }
+  }
+  
+  return edgeSet[row][0];
+}
+
+void checkIfTwice(vector<int> edges, bool arr[]) {
+  for(int i = 0; i < edges.size() - 1; i++) {
+    for(int j = 1; j < edges.size(); j++) {
+      if(edges[i] == edges[j] && i != j) {
+        arr[i] = true;
+        arr[j] = true;
+      }
+    }
+  }
+}
+
+
+void displayTree(vector< vector<int> > edgeSet, vector<int> T) {
+  cout << "Minimum Weight Spanning Tree\n";
+  
+  int totalW = 0;
+  
+  for(int i = 0; i < T.size(); i++) {
+    cout << "Edge " << edgeSet[T[i]][0] << " " << edgeSet[T[i]][1];
+    cout << " with weight " << edgeSet[T[i]][2] << endl;
+    totalW += edgeSet[T[i]][2];
+  }
+  
+  cout << "The total weight of this spanning tree is " << totalW << ".\n";
 }
